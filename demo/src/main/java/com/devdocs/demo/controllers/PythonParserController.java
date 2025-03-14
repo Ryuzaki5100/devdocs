f49d2dbea0acdbb6259b3d38d901abf0f0cba158
Atharva Gulkotwar <132251284+Ryuzaki5100@users.noreply.github.com>
2025-03-14 17:25:55 +0530

    Initial commit

--- demo/src/main/java/com/devdocs/demo/controllers/PythonParserController.java
+++ demo/src/main/java/com/devdocs/demo/controllers/PythonParserController.java
@@ -4,9 +4,7 @@ import com.devdocs.demo.utils.GitHubRepoContents;
 import com.devdocs.demo.utils.SimpleJavaParser;
 import com.devdocs.demo.utils.SimplePythonParser;
 import com.fasterxml.jackson.databind.JsonNode;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class PythonParserController {
@@ -16,6 +14,11 @@ public class PythonParserController {
         filePath = filePath.trim();
         System.out.println(filePath.length());
         String fileContent = GitHubRepoContents.getFileContents(owner, repo, branch, filePath);
+        return SimplePythonParser.parsePythonCode(fileContent);
+    }
+
+    @PostMapping("/rawParse")
+    public JsonNode rawParse(@RequestBody String fileContent) {
         return SimplePythonParser.parsePythonCode(fileContent);
     }
 }
